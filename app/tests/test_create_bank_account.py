f9494f8d3c179c8844ecc68e86d6183fde6aaa76
Kacper Kalinowski <k.kalinowski.732@studms.ug.edu.pl>
2022-10-29 19:11:53 +0200

    corrected lab2

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -8,40 +8,45 @@ class TestCreateBankAccount(unittest.TestCase):
         imie = "Dariusz"
         nazwisko = "Januszewski"
         poprawny_pesel = "55071284261"
-        niepoprawny_pesel = "666"
-        pesel_upowazniony_do_promocji = "65071284261"
-        kod_rabatowy = "PROM_XYZ"
-        pesel_po_2000 = "01234567898"
+        krotki_pesel = "666"
+        dlugi_pesel = "1234563546125512"
 
         pierwsze_konto = Konto(imie, nazwisko, poprawny_pesel)
-        konto_z_kodem = Konto(imie, nazwisko, pesel_upowazniony_do_promocji, kod_rabatowy)
-        konto_ze_zlym_peselem = Konto(imie, nazwisko, niepoprawny_pesel)
-        konto_ze_zlym_kodem = Konto(imie, nazwisko, poprawny_pesel, "ABC")
-        konto_seniora = Konto(imie, nazwisko, poprawny_pesel, kod_rabatowy)
-        konto_po_2000 = Konto(imie, nazwisko, pesel_po_2000, kod_rabatowy)
+        konto_z_krotkim_peselem = Konto(imie, nazwisko, krotki_pesel)
+        konto_z_dlugim_peselem = Konto(imie, nazwisko, dlugi_pesel)
 
-        #feature 1
         self.assertEqual(pierwsze_konto.imie, imie, "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, nazwisko, "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
 
-        #feature 2
         self.assertEqual(pierwsze_konto.pesel, poprawny_pesel, "Pesel nie został zapisany!")
 
-        #feature 3
         self.assertEqual(len(pierwsze_konto.pesel), 11, "Niepoprawny pesel!")
-        self.assertEqual(konto_ze_zlym_peselem.error, "Error", "Poprawny pesel nie zostal przyjety")
+        self.assertEqual(konto_z_dlugim_peselem.error, "Error", "Niepoprawny pesel zostal przyjety")
+        self.assertEqual(konto_z_krotkim_peselem.error, "Error", "Niepoprawny pesel zostal przyjety")
+    
+    def test_naliczanie_promocji_do_konta(self):
+        imie = "Dariusz"
+        nazwisko = "Januszewski"
+        pesel_seniora = "55071284261"
+        pesel_upowazniony_do_promocji = "65071284261"
+        kod_rabatowy = "PROM_XYZ"
+        pesel_po_2000 = "01234567898"
 
-        #feature 4
-        self.assertEqual(konto_z_kodem.kod_rabatowy, kod_rabatowy or None, "Niepoprawny kod rabatowy!")
-        self.assertNotEqual(konto_ze_zlym_kodem.kod_rabatowy, kod_rabatowy or None, "Przyjeto niepoprawny kod rabatowy!")
+        konto_bez_kodu = Konto(imie, nazwisko, pesel_po_2000)
+        konto_ze_zlym_kodem = Konto(imie, nazwisko, pesel_upowazniony_do_promocji, "ABC")
+        konto_upowaznione_do_promocji = Konto(imie, nazwisko, pesel_upowazniony_do_promocji, kod_rabatowy)    
+        konto_seniora = Konto(imie, nazwisko, pesel_seniora, kod_rabatowy)
+        konto_po_2000 = Konto(imie, nazwisko, pesel_po_2000, kod_rabatowy)
+        
+        self.assertEqual(konto_bez_kodu.kod_rabatowy, None, "Zapisano kod rabotwy inny niz None!")
+        self.assertEqual(konto_upowaznione_do_promocji.kod_rabatowy, kod_rabatowy, "Niezapisano kodu rabatowego!")
+        self.assertEqual(konto_ze_zlym_kodem.kod_rabatowy, "ABC", "Niezapisano kodu rabatowego!")
 
-        self.assertEqual(konto_z_kodem.saldo, 50, "Nienaliczono promocji!")
+        self.assertEqual(konto_upowaznione_do_promocji.saldo, 50, "Nienaliczono promocji!")
         self.assertEqual(konto_ze_zlym_kodem.saldo, 0, "Naliczono promocje ze zlym kodem!")
-        self.assertEqual(pierwsze_konto.saldo, 0, "Naliczono promocje bez kodu!")
+        self.assertEqual(konto_bez_kodu.saldo, 0, "Naliczono promocje bez kodu!")
 
-        #feature 5
-        self.assertEqual(konto_z_kodem.saldo, 50, "Nienaliczono promocji!")
         self.assertEqual(konto_seniora.saldo, 0, "Naliczono promocje seniorowi!")
         self.assertEqual(konto_po_2000.saldo, 50, "Nienlaiczono promocji po 2000!")
 
